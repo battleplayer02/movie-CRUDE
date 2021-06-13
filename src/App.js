01f2046b122b85d89ba7ca103477dcedf9cb38e0
battleplayer02 <himanshu00qwerty@gmail.com>
2021-06-13 12:18:45 +0530

    Added Movies component with table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
-import "./style.css";
-
-export default function App() {
+import Movies from './Components/Movies';
+import React from 'react';
+function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      <Movies />
+    </>
   );
 }
+export default App;
